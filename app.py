757f3dae65d337ed008e7e9eab3822b376fe95f1
farzad <farzad.panahi@gmail.com>
2020-12-15 03:04:35 -0800

    added crud methods

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-# Path hack !!! due to sibling package import troubles
-# import sys
-# import os
-# sys.path.insert(0, os.path.abspath('..'))
-
-from chalice import Chalice
+from chalice import Chalice, NotFoundError, ChaliceViewError, BadRequestError
 from chalicelib.geo_hash.geo_hash import GeoHash
 import configparser
 
@@ -11,7 +6,11 @@ app = Chalice(app_name='rest')
 
 config = configparser.ConfigParser()
 config.read('chalicelib/config.ini')
-
+geo_hash = GeoHash(
+    config['dynamodb']['region'],
+    config['dynamodb']['table'],
+    config['dynamodb']['hash_key_length']
+)
 
 @app.route('/')
 def index():
@@ -20,21 +19,65 @@ def index():
 
 @app.route('/pins')
 def get_pins():
-    geo_hash = GeoHash(
-        config['dynamodb']['region'],
-        config['dynamodb']['table'],
-        config['dynamodb']['hash_key_length']
-    )
-
     pins = geo_hash.get_geo_points()
+    if pins is None:
+        raise NotFoundError
+
     return [pin.to_dict() for pin in pins]
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+
+
+@app.route('/pin/{latitude}/{longitude}/{pinid}')
+def get_pin(latitude, longitude, pinid):
+    if not _validate_get_pin(latitude, longitude, pinid):
+        raise BadRequestError
+
+    pin = geo_hash.get_geo_point(float(latitude), float(longitude), pinid)
+    if pin is None:
+        raise NotFoundError
+
+    return pin.to_dict()
+
+
+@app.route('/pin', methods=['POST'])
+def post_pin():
+    data = app.current_request.json_body
+    if not _validate_post_pin(data):
+        raise BadRequestError
+
+    pin = geo_hash.add_geo_point(data['latitude'], data['longitude'], data['properties'])
+    if pin is None:
+        raise ChaliceViewError
+
+    return pin.to_dict()
+
+
+def _validate_post_pin(body):
+    if body is None or len(body) == 0:
+        return False
+
+    try:
+        latitude = float(body['latitude'])
+        longitude = float(body['longitude'])
+        properties = body['properties']
+
+        if len(properties) == 0:
+            return False
+
+    except:
+        return False
+
+    return True
+
+
+def _validate_get_pin(latitude, longitude, pinid):
+    try:
+        latitude = float(latitude)
+        longitude = float(longitude)
+
+        if pinid is None or len(pinid) == 0:
+            return False
+
+    except:
+        return False
+
+    return True
